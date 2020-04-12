e5baab64c363953b8e62e1b173a262e56c1eeb26
wynne-SE <52964964+wynne-SE@users.noreply.github.com>
2020-04-12 11:30:37 -0400

    Initial Commit

---
+++ adoptions/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+#Primary key is id field auto generated
+class Pet(models.Model):
+    SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    breed = models.CharField(max_length=30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
+    submission_date = models.DateTimeField
+    age = models.IntegerField(null=True)
+
+    #Foreign key for Vaccine Object (many to many)
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
+
+
+
